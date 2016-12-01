ed116868426051c7ece0ea2ced84c950228183db
Thillakan Saba <tsaba@itree.com.au>
2016-12-01 15:41:17 +1100

    Completed  ULTIPLE COMPONENTS https://angular.io/docs/ts/latest/tutorial/toh-pt3.html

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,21 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule}      from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {HeroDetailComponent} from './hero-detail.component';
 
-import { AppComponent }  from './app.component';
+import {AppComponent}  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    declarations: [
+        AppComponent,
+        HeroDetailComponent
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
